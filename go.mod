1d973d321e57cc207b52744f0198a012738b5cf1
Bjorn Leffler <leffler@google.com>
2023-06-12 21:56:47 -0700

    Add Metrics Exporter utility.

--- go.mod
+++ go.mod
@@ -11,12 +11,21 @@ require (
 require (
 	cloud.google.com/go/compute v1.19.3 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cakturk/go-netstat v0.0.0-20200220111822-e5b49efee7a5 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.10.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_golang v1.15.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/rafacas/sysstats v0.0.0-20150414182805-21d5ac1731f7 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
---
+++ metrics_exporter.go
@@ -0,0 +1,223 @@
+package main
+
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Metrics Exporter exports prometheus metrics to facilitate advanced load balancing.
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/netip"
+	"strconv"
+	"time"
+
+	"github.com/cakturk/go-netstat/netstat"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/rafacas/sysstats"
+	"golang.org/x/exp/slices"
+)
+
+const (
+	prefix   = "metrics_exporter_"
+	nfs4Port = 2049
+)
+
+var (
+	cpuUsagePercent = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: prefix + "cpu_usage_percent",
+		Help: "CPU usage in percent.",
+	})
+	memoryUsagePercent = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: prefix + "memory_usage_percent",
+		Help: "Memory usage in percent.",
+	})
+	systemLoad = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: prefix + "system_load",
+		Help: "System load (number of waiting threads).",
+	})
+	ingressTcpTotal = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: prefix + "ingress_tcp_connections_total",
+		Help: "Total number of ingress TCP connections.",
+	})
+	egressTcpTotal = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: prefix + "egress_tcp_connections_total",
+		Help: "Total number of egress TCP connections.",
+	})
+	ingressTcpByPort = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + "ingress_tcp_connections_by_port",
+		Help: "Total number of ingress TCP connections, per port",
+	}, []string{"port"})
+	egressTcpByPort = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + "egress_tcp_connections_by_port",
+		Help: "Number of egress TCP connections, per port.",
+	}, []string{"port"})
+	nfsConnections = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: prefix + "nfs_connections_total",
+		Help: "Total number of inbound NFS TCP connections.",
+	})
+	nfs4Connections = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: prefix + "nfs_v4_connections_total",
+		Help: "Total number of inbound NFSv4 TCP connections.",
+	})
+)
+
+func getCPUPercent() (float64, error) {
+	average, err := sysstats.GetCpuStatsInterval(1)
+	if err != nil {
+		return 0, err
+	}
+	// Use total used cpu as the indicator of how busy the system is.
+	cpu := average["cpu"]
+	// log.Printf("CPU Total: %v%%", cpu["total"])
+	return cpu["total"], nil
+}
+
+func getMemoryPercent() (float64, error) {
+	stats, err := sysstats.GetMemStats()
+	if err != nil {
+		return 0, err
+	}
+	used := float64(stats["memtotal"] - stats["realfree"])
+	usagePercent := 100.0 * used / float64(stats["memtotal"])
+	// log.Printf("Memory usage: %v%%", usagePercent)
+	return usagePercent, nil
+}
+
+func getLoad() (sysstats.LoadAvg, error) {
+	load, err := sysstats.GetLoadAvg()
+	if err != nil {
+		return load, err
+	}
+	return load, nil
+}
+
+func listLocalIPs() (ips []netip.Addr, err error) {
+	ifaces, err := net.Interfaces()
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return ips, err
+		}
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				if ip, ok := netip.AddrFromSlice(v.IP); ok {
+					ips = append(ips, ip)
+				}
+			case *net.IPAddr:
+				if ip, ok := netip.AddrFromSlice(v.IP); ok {
+					ips = append(ips, ip)
+				}
+			}
+		}
+	}
+	return ips, err
+}
+
+func getTcpCounts() (ingress, egress map[uint16]int64, err error) {
+	// Filter for established not loopback connections.
+	establishedNotLoopback := func(s *netstat.SockTabEntry) bool {
+		return s.State == netstat.Established && !s.LocalAddr.IP.IsLoopback()
+	}
+	// List established sockets.
+	socks4, err := netstat.TCPSocks(establishedNotLoopback)
+	if err != nil {
+		return ingress, egress, err
+	}
+	socks6, err := netstat.TCP6Socks(establishedNotLoopback)
+	if err != nil {
+		return ingress, egress, err
+	}
+	socks := append(socks4, socks6...)
+
+	localIPs, err := listLocalIPs()
+	if err != nil {
+		return ingress, egress, err
+	}
+
+	ingress = map[uint16]int64{}
+	egress = map[uint16]int64{}
+	for _, s := range socks {
+		ip, ok := netip.AddrFromSlice(s.LocalAddr.IP)
+		if !ok {
+			return ingress, egress, fmt.Errorf("Failed to parse %v", s.LocalAddr.IP)
+		}
+		if slices.Contains(localIPs, ip) {
+			egress[s.RemoteAddr.Port] += 1
+		} else {
+			ingress[s.LocalAddr.Port] += 1
+		}
+	}
+	return ingress, egress, nil
+}
+
+func exportMetrics() {
+	go func() {
+		for {
+			cpu, err := getCPUPercent()
+			if err != nil {
+				log.Printf("Error getting CPU usage: %v", err)
+			}
+			cpuUsagePercent.Set(cpu)
+
+			memory, err := getMemoryPercent()
+			if err != nil {
+				log.Printf("Error getting Memory usage: %v", err)
+			}
+			memoryUsagePercent.Set(memory)
+
+			load, err := getLoad()
+			if err != nil {
+				log.Printf("Error getting Load value: %v", err)
+			}
+			systemLoad.Set(load.Avg1)
+
+			// TCP connections.
+			ingress, egress, err := getTcpCounts()
+			if err != nil {
+				log.Printf("Error getting Load value: %v", err)
+			}
+			ingressTotal, egressTotal := 0, 0
+			for k, v := range ingress {
+				ingressTotal += 1
+				port := strconv.FormatUint(uint64(k), 10)
+				ingressTcpByPort.WithLabelValues(port).Set(float64(v))
+			}
+			for k, v := range egress {
+				egressTotal += 1
+				port := strconv.FormatUint(uint64(k), 10)
+				egressTcpByPort.WithLabelValues(port).Set(float64(v))
+			}
+			ingressTcpTotal.Set(float64(ingressTotal))
+			egressTcpTotal.Set(float64(egressTotal))
+			nfs4 := float64(ingress[nfs4Port])
+			nfs4Connections.Set(nfs4)
+			nfsConnections.Set(nfs4)
+
+			time.Sleep(15 * time.Second)
+		}
+	}()
+}
+
+func main() {
+	log.Printf("Start Metrics Exporter")
+	exportMetrics()
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
